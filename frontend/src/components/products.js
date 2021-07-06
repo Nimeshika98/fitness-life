07513d934b6b7f67601c510a18e8d444a55024be
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2021-07-07 03:29:20 +0530

    cart

--- frontend/src/components/products.js
+++ frontend/src/components/products.js
@@ -4,7 +4,7 @@ export const products = [
       title: "20kg DUMBBELL SET",
       description: "Silver in color",
       price: "7500",
-      img: "./images/dumbbell1.jpg",
+      img: "./images/equipment/dumbells/dumbbell1.jpg",
       quantity: 1,
     },
     {
@@ -12,7 +12,7 @@ export const products = [
       title: "10kg DUMBBELL SET",
       description: "Black in color",
       price: "5500",
-      img: "./images/dumbbell2.jpg",
+      img: "./images/equipment/dumbells/dumbbell2.jpg",
       quantity: 1,
     },
     {
@@ -21,7 +21,7 @@ export const products = [
       //The solid and thick material ensures a comfortable lying down posture during your floor exercises. green in color
       description: "The solid and thick material ensures a comfortable... \n green in color",
       price: "3500",
-      img: "./images/mat1.jpg",
+      img: "./images/equipment/mats/mat1.jpg",
       quantity: 1,
     },
     {
@@ -30,7 +30,7 @@ export const products = [
       //The solid and thick material ensures a comfortable lying down posture during your floor exercises. black in color
       description: "The solid and thick material ensures a comfortable...\n black in color",
       price: "5800",
-      img: "./images/mat2.jpg",
+      img: "./images/equipment/mats/mat2.jpg",
       quantity: 1,
     },
     
