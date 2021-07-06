c68cd5ff0071568891e3926f1724b43f9f0deb1c
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2021-07-07 03:28:39 +0530

    index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 //import './App.css';
-import './components/cart.css';
+//import './components/cart.css';
 //import Header from "./components/header";
 //import Footer from "./components/footer";
 import Cart from "./components/Cart";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ViewProduct from "./components/viewProduct";
 
 function App() {
   return (
     <div> 
-      <Cart/>
+      <ViewProduct/>
     </div>
   );
 }
